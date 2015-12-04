ea89a7f964a21dd8ed4b3ecb725c0723bc20face
Dimitris Baltas <dbaltas@travelplanet24.com>
2015-12-04 14:09:42 +0200

    fixtures: api sprint.get for fake provider request and response

---
+++ app/fixtures/sprint.get/response.js
@@ -0,0 +1,15 @@
+{
+	"result": {
+		"name" : "Sprint 1",
+		"id" : "101",
+		"squad" : "kunata team".
+		"PBI" : [
+			{"title" : "create json samples", "status" : "Done", "bv" : 3, "sp" : 5, "theme" : "N/A", "epic" : "Monday Standup"},
+			{"title" : "create repo", "status" : "Accepted", "bv" : 5, "sp" : 0, "theme" : "N/A", "epic" : "Monday Standup"},
+			{"title" : "html", "status" : "In Progress", "bv" : 13, "sp" : 8, "theme" : "N/A", "epic" : "Monday Standup"},
+			{"title" : "backbone models", "status" : "In Progress", "bv" : 8, "sp" : 13, "theme" : "N/A", "epic" : "Monday Standup"}
+		]
+	},
+	"status": 200,
+	"error": {}
+}
